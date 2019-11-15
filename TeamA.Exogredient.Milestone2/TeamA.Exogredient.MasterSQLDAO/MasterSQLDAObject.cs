0e13c8f95eb9939bcb6422f1545c2212da5f2136
EleazarGomez <elithegolfer@gmail.com>
2019-11-15 13:50:40 -0800

    Master SQL Interface Co-authored-by: Jason7539 <Jnguyen7539@gmail.com>

---
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.MasterSQLDAO/MasterSQLDAObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamA.Exogredient.MasterSQLDAO
+{
+    public interface IMasterSQLDAObject<T>
+    {
+        public static readonly string _connectionString = "sdf";
+
+        // Create a record in the data store based on the model argument.
+        public bool Create(Object record);
+
+        // Deletes all rows based on each primary key id in the argument list.
+        public bool DeleteByIDs(List<T> idsOfRows);
+
+        // Reads all rows based on each primary key id in the argument list.
+        public List<Object> ReadByIDs(List<T> idsOfRows);
+
+        // Updates the row marked by the primary key id using the record object.
+        // The "record" object defines default values for every column of the table,
+        // the user passes named values to each column they wish to update.
+        // Example: record = User(lastName : "Example")
+        //          Update("jason1234", record)
+        public bool Update(T id, Object record);
+    }
+}
