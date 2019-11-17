4a59fb951462c6d903409e7c108c53556ce08727
Jason Nguyen <jnguyen7539@gmail.com>
2019-11-16 20:39:34 -0800

    created MasterNOSQLDAO abstract class

---
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.DAL/MasterNOSQLDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeamA.Exogredient.DAL
+{
+    public abstract class MasterNOSQLDAO<T>
+    {
+        protected static readonly string ConnectionString = "mysqlx://root:****@localhost:3306";
+
+        public abstract void Create(string json);
+
+        public abstract void Read(T uniqueID);
+
+        public abstract void Delete(T uniqueID);
+    }
+}
