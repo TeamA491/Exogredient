1f1983e774a0be576d0b88d0f5ed7c0534e2d73e
Jason Nguyen <jnguyen7539@gmail.com>
2019-11-21 14:08:20 -0800

    Removed incorrect instantiation of LoggingManager in LoggingController

--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.LoggingController/LoggingController.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.LoggingController/LoggingController.cs
@@ -7,10 +7,6 @@ namespace TeamA.Exogredient.LoggingController
     {
         static void Main(string[] args)
         {
-            LoggingManager manager = new LoggingManager();
-
-            manager.Log(null, new DateTime(), null, null, null, null);
-
         }
     }
 }
