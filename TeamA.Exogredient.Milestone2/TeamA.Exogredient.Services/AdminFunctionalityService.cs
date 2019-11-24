cef2210b6b2a198e5eece9ca23b6d029fbb3140f
EleazarGomez <elithegolfer@gmail.com>
2019-11-24 11:25:38 -0800

    Notify sys admin update, removed password

--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/AdminFunctionalityService.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/AdminFunctionalityService.cs
@@ -8,6 +8,18 @@ namespace TeamA.Exogredient.Services
 {
     public class AdminFunctionalityService
     {
+        private readonly string _sendingEmail = "exogredient.system@gmail.com";
+
+        // HACK: replace with actual password
+        private readonly string _sendingEmailPassword = "****";
+
+        private readonly string _receivingEmail = "TEAMA.CS491@gmail.com";
+
+        /// <summary>
+        /// Uses gmail smtp to send mail from a known email address to any other address.
+        /// </summary>
+        /// <param name="message">The message you want to send.</param>
+        /// <returns>A bool representing whether the process succeeded.</returns>
         public async Task<bool> NotifySystemAdminAsync(string message)
         {
             try
@@ -15,13 +27,13 @@ namespace TeamA.Exogredient.Services
                 MailMessage mail = new MailMessage();
                 SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-                mail.From = new MailAddress("exogredient.system@gmail.com");
-                mail.To.Add("TEAMA.CS491@gmail.com");
+                mail.From = new MailAddress(_sendingEmail);
+                mail.To.Add(_receivingEmail);
                 mail.Subject = "Test Mail";
                 mail.Body = $"{message}";
 
                 SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("exogredient.system@gmail.com", "z8Bids@l%dkHF!");
+                SmtpServer.Credentials = new System.Net.NetworkCredential(_sendingEmail, _sendingEmailPassword);
                 SmtpServer.EnableSsl = true;
 
                 await SmtpServer.SendMailAsync(mail);
