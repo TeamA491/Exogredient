b5869b971c9adb8c32d18e633f16709ed2ac8f46
SuperMarioMemes <36057662+SuperMarioMemes@users.noreply.github.com>
2019-12-01 12:59:58 -0800

    Changes to CompressionService, and Unit Tests for Compression, File Fetching and FTP
    Fixed logic error in compressionservice involving checking valid file paths. Wrote unit tests for File Fetching and Compression.

--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/CompressionService.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/CompressionService.cs
@@ -8,7 +8,7 @@ namespace TeamA.Exogredient.Services
         public static bool Compress(string sevenZipPath, string sourceDirectory, string targetDirectory)
         {
             // Check to make sure source Directory exists
-            if (!(Directory.Exists(sourceDirectory) || Directory.Exists(targetDirectory) || File.Exists(sevenZipPath)))
+            if (!(Directory.Exists(sourceDirectory) && Directory.Exists(targetDirectory) && File.Exists(sevenZipPath)))
             {
                 return false;
             }
--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.Tests/FileFetchingServiceTests.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Tests/FileFetchingServiceTests.cs
@@ -1,10 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamA.Exogredient.Services;
+using System.IO;
+using System;
 
 namespace TeamA.Exogredient.Tests
 {
-    class FileFetchingServiceTests
+    [TestClass]
+    public class FileFetchingServiceTests
     {
+        [TestMethod]
+      public void FileFetchingService_FetchLogs_FetchExistingFilesToNewDirectory()
+        {
+            //arrage
+            DateTime currentTime = DateTime.Now;
+            string sourceDirectory = @"C:\_LogFiles\TF3";
+            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
+            int days = 0;
+
+            // If source directory doesn't exist create it.
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Directory.CreateDirectory(sourceDirectory);
+            }
+
+            // If sourceDirectory doesn't contain any logs create them.
+            if (Directory.GetFiles(sourceDirectory).Length == 0)
+            {
+                using (File.Create(sourceDirectory + @"\log1.csv")) { } ;
+            }
+
+            // Delete targetdirectory to test FetchExistingFilesToNewDirectory
+            if (Directory.Exists(targetDirectory))
+            {
+                Directory.Delete(targetDirectory, true);
+            }
+
+            //act
+            bool result = FileFetchingService.FetchLogs(sourceDirectory,targetDirectory,days);
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void FileFetchingService_FetchLogs_NoLogFilesinSourceDirectory()
+        {
+            DateTime currentTime = DateTime.Now;
+            string sourceDirectory = @"C:\ajdshfkjahds";
+            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
+
+            // If source Directory exists delete it and recreate so it has no logs files.
+            if (Directory.Exists(sourceDirectory))
+            {
+                Directory.Delete(sourceDirectory, true);
+                Directory.CreateDirectory(sourceDirectory);
+            }
+            // If source Directory doesn't exist create it.
+            else
+            {
+                Directory.CreateDirectory(sourceDirectory);
+            }
+            //act
+            bool result = FileFetchingService.FetchLogs(sourceDirectory,targetDirectory,30);
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void FileFetchingService_FetchLogs_FetchToExistingDirectory()
+        {
+            DateTime currentTime = DateTime.Now;
+            string sourceDirectory = @"C:\_LogFiles\TF1";
+            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
+
+            // If source directory doesn't exist create it.
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Directory.CreateDirectory(sourceDirectory);
+            }
+
+            // If sourceDirectory doesn't contain any logs create them.
+            if (Directory.GetFiles(sourceDirectory).Length == 0)
+            {
+                using (File.Create(sourceDirectory + @"\log1.csv")) { }
+            }
+
+            // Create target directory if it doesn't exists to test FetchToExistingDirectory.
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            //act
+            bool result = FileFetchingService.FetchLogs(sourceDirectory,targetDirectory,0);
+
+            //assert 
+            Assert.IsTrue(result);
+        }
     }
 }
