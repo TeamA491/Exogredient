f04fad4ade16d88bcfd95a965f9c39400193c688
EleazarGomez <elithegolfer@gmail.com>
2019-12-06 11:24:12 -0800

    Made FTP service async

--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/FTPService.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/FTPService.cs
@@ -1,14 +1,13 @@
 ﻿using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace TeamA.Exogredient.Services
 {
     public static class FTPService
     {
-        public static bool Send(string ftpURL, string ftpFolder, string filePath, string sourceDirectory, string userName, string password)
+        public static async Task<bool> SendAsync(string ftpURL, string ftpFolder, string filePath, string sourceDirectory, string userName, string password)
         {
-            byte[] fileBytes = null;
-
             // File to send on local machine.
             string archiveFilePath = sourceDirectory + ".7z";
             if (!File.Exists(archiveFilePath))
@@ -16,33 +15,28 @@ namespace TeamA.Exogredient.Services
                 return false;
             }
 
-            fileBytes = File.ReadAllBytes(archiveFilePath);
+            byte[] fileBytes = File.ReadAllBytes(archiveFilePath);
 
-            try
+
+            // Create the FTP request. Set the folder and file name.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpURL + ftpFolder + Path.GetFileName(filePath));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // Entering in FTP user credentials.
+            request.Credentials = new NetworkCredential(userName, password);
+
+            // Specify additional information about the FTP request.
+            request.ContentLength = fileBytes.Length;
+            request.UsePassive = true;
+            request.UseBinary = false;
+            request.EnableSsl = false;
+
+            using (Stream requestStream = await request.GetRequestStreamAsync().ConfigureAwait(false))
             {
-                // Create the FTP request. Set the folder and file name.
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpURL + ftpFolder + Path.GetFileName(filePath));
-                request.Method = WebRequestMethods.Ftp.UploadFile;
-
-                // Entering in FTP user credentials.
-                request.Credentials = new NetworkCredential(userName, password);
-
-                // Specify additional information about the FTP request.
-                request.ContentLength = fileBytes.Length;
-                request.UsePassive = true;
-                request.UseBinary = false;
-                request.EnableSsl = false;
-
-                using (Stream requestStream = request.GetRequestStream())
-                {
-                    requestStream.Write(fileBytes, 0, fileBytes.Length);
-                    requestStream.Close();
-                }
+                await requestStream.WriteAsync(fileBytes, 0, fileBytes.Length).ConfigureAwait(false);
+                requestStream.Close();
             }
-            catch (WebException ex)
-            {
-                return false;
-            }
+
             return true;
         }
     }
