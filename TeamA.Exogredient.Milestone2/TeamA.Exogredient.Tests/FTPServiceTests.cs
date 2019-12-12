861e536bcb31230d3b90e7f06837d7def028df2f
SuperMarioMemes <36057662+SuperMarioMemes@users.noreply.github.com>
2019-12-12 12:56:16 -0800

    Fixes to FTPServiceTests

--- TeamA.Exogredient.Milestone2/TeamA.Exogredient.Tests/FTPServiceTests.cs
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Tests/FTPServiceTests.cs
@@ -17,15 +17,27 @@ namespace TeamA.Exogredient.Tests
         {
             // Arrange
             DateTime currentTime = DateTime.Now;
-
-            // Create the target. 
-            Directory.CreateDirectory(@"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy"));
+            string targetDirectory = @"C:\_ArchiveFiles\TF1\" + currentTime.ToString("ddMMyy");
             
-
-            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
+            // Create a base archive directory with another directory inside of it(named after date). 
+            if (!Directory.Exists(@"C:\_ArchiveFiles\TF1\"))
+            {
+                Directory.CreateDirectory(@"C:\_ArchiveFiles\TF1\");
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+            }
+            // List the path to archive file. 
+            string targetFile = targetDirectory + Constants.SevenZipFileExtension;
+            if (!File.Exists(targetFile))
+            {
+                File.Create(targetFile);
+            }
+            // Set up FTP credentials
             string ftpUrl = Constants.FTPUrl;
-            string userName = "Archiver";
-            string password = "onlyicanpass5%";
+            string userName = Constants.FTPUsername;
+            string password = Constants.FTPpassword;
             
             // Act
             bool results = await FTPService.SendAsync(ftpUrl, "", targetDirectory, userName, password);
@@ -35,7 +47,7 @@ namespace TeamA.Exogredient.Tests
 
 
             // Cleanup 
-            Directory.Delete(@"C:\_ArchiveFiles\", true);
+            //Directory.Delete(@"C:\_ArchiveFiles\TF1", true);
 
         }
 
@@ -44,7 +56,22 @@ namespace TeamA.Exogredient.Tests
         {
             // Arrange
             DateTime currentTime = DateTime.Now;
-            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
+            string targetDirectory = @"C:\_ArchiveFiles\TF2\" + currentTime.ToString("ddMMyy");
+            // Create a test folder with another directory inside named after the date. 
+            if (!Directory.Exists(@"C:\_ArchiveFiles\TF2\"))
+            {
+                Directory.CreateDirectory(@"C:\_ArchiveFiles\TF2\");
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+            }
+            // Create variable for archive file inside directories. 
+            string targetFile = targetDirectory + Constants.SevenZipFileExtension;
+            if (!File.Exists(targetFile))
+            {
+                File.Create(targetFile);
+            }
             string ftpUrl = Constants.FTPUrl;
             string userName = Constants.FTPUsername;
             bool results;
@@ -62,6 +89,9 @@ namespace TeamA.Exogredient.Tests
             }
             // Assert
             Assert.IsFalse(results);
+            
+            // Cleanup
+            //Directory.Delete(@"C:\_ArchiveFiles\TF2\", true);
         }
 
         [TestMethod]
@@ -69,23 +99,36 @@ namespace TeamA.Exogredient.Tests
         {
             // Arrange
             DateTime currentTime = DateTime.Now;
-            string targetDirectory = @"C:\_ArchiveFiles\" + currentTime.ToString("ddMMyy");
-
+            string targetDirectory = @"C:\_ArchiveFiles\TF3\" + currentTime.ToString("ddMMyy");
+            
+            // Create test directories. 
+            if (!Directory.Exists(@"C:\_ArchiveFiles\TF3\"))
+            {
+                Directory.CreateDirectory(@"C:\_ArchiveFiles\TF3\");
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+            }
+            
+            // Create dummy archive file. 
+            string targetFile = targetDirectory + Constants.SevenZipFileExtension;
+            if (!File.Exists(targetFile))
+            {
+                File.Create(targetFile);
+            }
             string ftpUrl = Constants.FTPUrl;
             string userName = Constants.FTPUsername;
-            bool results;
-            
-            // Attempting to transfer file with wrong password to ftp server
             string password = Constants.FTPpassword;
-            string targetFilePath = targetDirectory + Constants.SevenZipFileExtension;
+            bool results;
 
             // Delete the file we want to send to remote to test SendWithoutTargetFile.
-            File.Delete(targetFilePath);
+            File.Delete(targetFile);
 
             // Act
             try
             {
-                results = await FTPService.SendAsync(ftpUrl, "", targetDirectory, userName, password);
+                results = await FTPService.SendAsync(ftpUrl, "", "", userName, password).ConfigureAwait(false);
             }
             catch(ArgumentException e)
             {
@@ -94,6 +137,9 @@ namespace TeamA.Exogredient.Tests
 
             // Assert
             Assert.IsFalse(results);
+            
+            // Cleanup
+            //Directory.Delete(@"C:\_ArchiveFiles\TF3\", true);
         }
 
     }
