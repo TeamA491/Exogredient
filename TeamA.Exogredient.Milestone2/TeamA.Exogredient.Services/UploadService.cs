5ff318237aef2649a98229c0e253d980d15409e1
Jason Nguyen <jnguyen7539@gmail.com>
2020-02-03 14:00:09 -0800

    added files for Upload Functionality

---
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/UploadService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamA.Exogredient.Services
+{
+    public class UploadService
+    {
+        public async Task<bool> CreateUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveProgessUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteProgressUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ContinueProgressUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ReportUpload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
