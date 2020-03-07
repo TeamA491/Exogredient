0f0c2f4efe1e24b4c58fd8cfeb1e5d87e1d65c6d
Jason Nguyen <jnguyen7539@gmail.com>
2020-03-06 17:04:11 -0800

    created files and method signatures for profile functionality

---
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Managers/UserProfileManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamA.Exogredient.Managers
+{
+    public class UserProfileManager
+    {
+        public string GetProfileScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSavedUploadDraft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSaveList()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ TeamA.Exogredient.Milestone2/TeamA.Exogredient.Services/SaveListService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamA.Exogredient.Services
+{
+    public class SaveListService
+    {
+        public string GetSaveList(string user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
