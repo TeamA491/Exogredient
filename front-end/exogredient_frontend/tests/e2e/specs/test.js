df78f3fed7eab3b29ccdbb53bc22d062d4906e3c
Charles Kwak <charles971026@gmail.com>
2020-03-27 15:59:32 -0700

    End-to-End test started

--- front-end/exogredient_frontend/tests/e2e/specs/test.js
+++ front-end/exogredient_frontend/tests/e2e/specs/test.js
@@ -1,8 +1,28 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+describe('Search Test', () => {
+  it('Search by Ingredient', () => {
+    cy.visit("http://localhost:8080/")
+    cy.get('input[name="street"]').type("1250");
+    cy.wait(600);
+    cy.get('html').click(400,135);
+    cy.get('input[name="search"]').type("beef");
+    cy.get('input[name="mile"]').type(100);
+    cy.contains('Ingredient').click();
+    cy.contains('Search').click();
+    cy.get('tr').should('have.length',4);
   })
+
+  it("Clicking a store returns correct num of ingredients ", ()=>{
+    
+    cy.get(".table tr:nth-child(2) td:nth-child(2)").then(($td)=>{
+      const ingredientNum = $td.text();
+      cy.get(".table tr:nth-child(2) td:nth-child(1) a").click();
+      cy.wait(2000);
+      cy.get(".table tr").its('length').should('eq',parseInt(ingredientNum)+1);
+    })
+  })
+
+
+
 })
