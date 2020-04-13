1ff9a78b8265542c01ee96230dafaad9d15f8cce
Charles Kwak <charles971026@gmail.com>
2020-04-13 13:36:37 -0700

    Frontend comments and code clean up

--- front-end/exogredient_frontend/public/testData.js
+++ front-end/exogredient_frontend/public/testData.js
@@ -1,7 +1,3 @@
-// let ingredients = ["apple", "asparagus", "banana", "bean", "beet", "bell pepper", "blueberry", "broccoli", 
-// "cabbage", "carrot", "cheese", "cherry", 'corn', 'cucumber', 'egg', 'grape', 'kale', 'kiwi', 'mango', 'milk', 'pear'];
-// let ingredients = ['pineapple','pumpkin','rice','salmon','spinach','strawberry','tofu','tomato',
-// 'tuna','turkey','watermelon','wheat','yogurt','bacon','butter','salt','garlic','onion','soy sauce','sugar','honey','celery'];
 let ingredients = ["apple", "asparagus", "banana", "bean", "beet", "bell pepper", "blueberry", "broccoli", 
 "cabbage", "carrot", "cheese", "cherry", 'corn', 'cucumber', 'egg', 'grape', 'kale', 'kiwi', 'mango', 'milk', 'pear',
 'mushroom','lettuce','yam','jalapeno','jackfruit','horseradish','pineapple','pumpkin','rice','salmon','spinach','strawberry','tofu','tomato',
@@ -10,9 +6,13 @@ let ingredients = ["apple", "asparagus", "banana", "bean", "beet", "bell pepper"
 'rosemary','paprika','mint','cream','cheddar cheese','ricotta cheese','parmesan cheese','blue cheese','mozzarella cheese', 'papaya',
 'orange','olive','lemon','lychee','date','coconut','peach','apricot'];
 let selectedStores = new Set();
-let selectedIngredients = new Set();
+let selectedIngredients = new Set()
+
+// Change accordingly to the number of stores in DB
 let totalStoresNum = 21;
 let totalIngredientsNum = ingredients.length;
+
+// Change accordingly to the last data stored in DB
 let minute = 5;
 let second = 27;
 
@@ -21,17 +21,23 @@ function getRandomInt(max) {
 }
 
 while(selectedStores.size < totalStoresNum || selectedIngredients.size < totalIngredientsNum){
+    // Choose random store, random ingredient, and random price. 
     let storeId = getRandomInt(totalStoresNum)+1;
     let ingredient = ingredients[getRandomInt(totalIngredientsNum)];
     let price = getRandomInt(99)+1;
+
+    // Add to the randomly selected store and ingredient into each list.
     selectedStores.add(storeId);
     selectedIngredients.add(ingredient);
 
+    // Time Stamp formatting
     let minuteString = minute > 9 ? minute.toString(): '0'+minute.toString();
     let secondString = second > 9 ? second.toString(): '0'+second.toString();
 
+    // SQL statement
     console.log(`insert into upload values (null,${storeId},'${ingredient}','username','2020-01-01 00:${minuteString}:${secondString}','description','5','photo',${price},0,0,0);`);
 
+    // Update the time stamp
     second += 1;
     if(second === 60){
         second = 0;
--- front-end/exogredient_frontend/src/views/StoreView.vue
+++ front-end/exogredient_frontend/src/views/StoreView.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="storeView">
         <p><a @click="openMap">{{storeViewData.storeName}}</a></p>
-        <img :src="storeImage" width="100" height="100">
+        <img :src="storeImage" @error="storeImageLoadError=true" width="100" height="100">
         <p>{{storeViewData.storeDescription}}</p>
         <table class = "table is-fullwidth">
             <tr>
@@ -28,38 +28,56 @@
 </template>>
 
 <script>
-import {resultsPerPage} from '../main.js'
+import * as global from '../globalExports.js';
+import defaultStoreImage from "../assets/defaultStoreImage.jpeg";
+
 export default {
     name: "StoreView",
     mounted(){
         let currentPage = this.$store.state.currentPages.storeView;
         this.$refs.paginations[currentPage-1].classList.add("is-current");
     },
+    data(){
+        return{
+            storeImageLoadError: false,
+        }
+    },
     computed:{
         resultsPerPage: function(){
-            return resultsPerPage;
+            return global.ResultsPerPage;
         },
+
         ingredients: function(){
             return this.$store.getters.ingredientResults;
         },
+
         ingredientResultsTotalNum: function(){
             return this.$store.state.totalResultsNum.ingredientResultsTotalNum;
         },
+        
         storeViewData: function(){
             return this.$store.state.storeViewData;
         },
+
         storeImage: function(){
-            return `https://localhost:5001/api/search/image?id=${this.storeViewData.storeId}`;
+            var storeImage =`${global.ApiDomainName}/api/search/storeImage?`
+                + `storeId=${this.storeViewData.storeId}&username=${this.$store.state.username}`
+                + `&ipAddress=${this.$store.state.ipAddress}`;
+
+            return this.$data.storeImageLoadError? defaultStoreImage : storeImage;
         },
-         currentPage: function(){
+
+        currentPage: function(){
             return this.$store.state.currentPages.storeView;
         },
+
         isPreviousButtonDisabled: function(){
             if(this.currentPage === 1){
                 return true;
             }
             return false;
         },
+
         isNextButtonDisabled: function(){
             if(this.currentPage === Math.ceil(this.ingredientResultsTotalNum/this.resultsPerPage)){
                 return true;
@@ -68,34 +86,60 @@ export default {
         }
     },
     methods:{
+
         displayIngredientView: function(){
             //TODO: Implement what to do when user clicks the ingredient.
         },
+
         openMap: function(){
+            // Opens Google Maps direction to the store from the center of search.
             var encodedOriginAddress = encodeURIComponent(this.$store.state.searchData.address);
-            window.open(`https://www.google.com/maps/dir/?api=1&origin=${encodedOriginAddress}&destination=${this.storeViewData.storeName}&destination_place_id=${this.storeViewData.placeId}&travelmode=driving`, "_blank");
+            window.open(`https://www.google.com/maps/dir/?api=1`
+                + `&origin=${encodedOriginAddress}&destination=${this.storeViewData.storeName}`
+                + `&destination_place_id=${this.storeViewData.placeId}&travelmode=driving`, "_blank");
         },
+
         updatePagination: function(newPage){
+            // Remove "is-current" class from the anchor of the current page.
             this.$refs.paginations[this.currentPage-1].classList.remove("is-current");
+            // Add "is-current" class to the anchor of the new page.
             this.$refs.paginations[newPage-1].classList.add("is-current");
+            // Update SearchResultsView's current page in Vuex.
             this.$store.dispatch('updateStoreViewCurrentPage', newPage);
         },
+
         movePage: async function(newPage){
+            // Compute how many pages are being skipped.
             let skipPages = newPage - this.currentPage;
+
+            // Check if the current page is clicked.
             if(skipPages === 0){
                 return;
             }
+            
+            // Get the ingredient name of the last store in this current page.
             let lastIngredientName = this.ingredients[this.ingredients.length-1].ingredientName;
-            let ingredientResultsResponse = await fetch(`https://localhost:5001/api/search/getIngredientResults?` +
-            `username=${this.$store.state.username}&ipAddress=${this.$store.state.ipAddress}&storeId=${this.$store.state.storeViewData.storeId}&skipPages=${skipPages}`
-            + `&lastPageResultsNum=${this.ingredients.length}&lastIngredientName=${lastIngredientName}`
-            + `&ingredientName=${this.$store.state.searchData.searchBy === 'ingredient'? this.$store.state.searchData.searchTerm:""}`);
 
+            // Fetch list of ingredients for the new page.
+            let ingredientResultsResponse = 
+                await fetch(`${global.ApiDomainName}/api/search/getIngredientResults?` 
+                + `username=${this.$store.state.username}&ipAddress=${this.$store.state.ipAddress}`
+                + `&storeId=${this.$store.state.storeViewData.storeId}&skipPages=${skipPages}`
+                + `&lastPageResultsNum=${this.ingredients.length}&lastIngredientName=${lastIngredientName}`
+                + `&ingredientName=${this.$store.state.searchData.searchBy === global.SearchByIngredient?
+                 this.$store.state.searchData.searchTerm:""}`);
+        
+            // Handle any error from the HTTP status code.
+            global.ErrorHandler(this.$router, ingredientResultsResponse);
+
+            // Get the list of ingredients.
             let ingredientResults = await ingredientResultsResponse.json();
+            
+            // Update the list of ingredients in Vuex.
             this.$store.dispatch('updateIngredientResults', ingredientResults);
+            
             this.updatePagination(newPage);
         },
-
     }
 }
 </script>
