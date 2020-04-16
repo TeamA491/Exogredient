d06fad6e8e0eb2dfafecfbc693d69518e24f2691
Jason Nguyen <jnguyen7539@gmail.com>
2020-04-16 13:48:24 -0700

    added .csv of test data and sql script for unit tests

---
+++ Docs/Profile Docs/TestData/undo_delete.sql
@@ -0,0 +1,5 @@
+
+# RUN AFTER TESTS 
+-- INSERT INTO `exogredient`.`upload` (`upload_id`, `store_id`, `ingredient_name`, `uploader`, `post_time_date`, `description`, `rating`, `photo`, `price`, `upvote`, `downvote`, `in_progress`) VALUES ('20', '1', 'asdfasdf', 'username', '2020-05-03 00:00:00', 'asdf', '3', 'photo', '10.', '0', '0', '0');
+-- INSERT INTO `exogredient`.`save_list` (`ingredient`, `store`, `username`) VALUES ('beef', '1', 'username');
+-- INSERT INTO `exogredient`.`save_list` (`ingredient`, `store`, `username`) VALUES ('beef', '1', 'zero');
